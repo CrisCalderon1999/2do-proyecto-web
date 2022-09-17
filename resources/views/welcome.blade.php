6bee65a46beafed7a8f4df9acdbd2de9f6a5a301
crisCH <cris123868@gmail.com>
2022-09-17 12:24:06 -0600

    segundo commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,8 +13,8 @@
     <p class="card-text">Ingrese su opcion</p>
     <a href="#" class="btn btn-primary">Inventario</a>
     <h1>hola </h1>
-    <h1>hola </h1>
-    <h1>hola </h1>
+    <h1>hola como estas</h1>
+    <h1>hola como estas</h1>
     <h1>hola </h1>
   </div>
 </div>
